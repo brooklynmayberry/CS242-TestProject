741827abc8b64cdcc70a9a32eaf5c148f4ee7c82
BMustico <musticbt@clarkson.edu>
2022-09-28 16:14:46 -0400

    Added an array in the main method.

--- src/HelloMain.java
+++ src/HelloMain.java
@@ -5,5 +5,13 @@ public class HelloMain {
         Say something = new SayHello();
         System.out.println("Hola, World");
         something.saySomething();
+
+        int[] array = {1,2,3,4,5};
+        int arraySum = 0;
+        for (int i : array) {
+            arraySum += i;
+        }
+
+        System.out.println(arraySum);
     }
 }
