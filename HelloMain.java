63c5c94631a2e4aa475578ff2297ba722f562a09
BMustico <musticbt@clarkson.edu>
2022-09-28 15:47:47 -0400

    I made hello world have an abstract class... Really it's kind of an abstract idea...

--- HelloMain.java
+++ HelloMain.java
@@ -1,7 +1,7 @@
 public class HelloMain {
 
     public static void main(String[] args) {
-        System.out.println("Inheritance!");
+        System.out.println("Hello, Inheritance!");
         Say something = new SayHello();
         something.saySomething();
     }
