3497ddd9e09d25c98c28f68260b40e3946637a00
brooklynmayberry <mayberbd@clarkson.edu>
2022-09-28 15:57:39 -0400

    My second Commit

--- src/HelloMain.java
+++ src/HelloMain.java
@@ -3,6 +3,7 @@ public class HelloMain {
     public static void main(String[] args) {
         System.out.println("Hello, Inheritance!");
         Say something = new SayHello();
+        System.out.println("Hola, World");
         something.saySomething();
     }
 }
